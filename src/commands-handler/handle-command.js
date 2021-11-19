5ee3c5faac6eb737b0554126bede0d51f4bd573e
Dmitry Lytov <dmitrycmc@yandex-team.ru>
2021-11-19 22:56:17 +0300

    Bad command fix

--- src/commands-handler/handle-command.js
+++ src/commands-handler/handle-command.js
@@ -4,17 +4,23 @@ const handleCommand = async ({text, userId, username, firstname, lastname, chatI
     let [command, ...args] = text.split(' ');
     command = command.split(/[/@]/)[1];
 
-    const commandHandler = require(`./commands/${command}`);
+    try {
+        const commandHandler = require(`./commands/${command}`);
 
-    await telegramProvider.send({text: await commandHandler({
-        text,
-        userId,
-        username,
-        firstname,
-        lastname,
-        chatId,
-        args
-    }), chatId});
+        await telegramProvider.send({
+            text: await commandHandler({
+                text,
+                userId,
+                username,
+                firstname,
+                lastname,
+                chatId,
+                args
+            }), chatId
+        });
+    } catch (e) {
+
+    }
 };
 
 module.exports = handleCommand;
